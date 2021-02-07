a2c721bb497fdc72b4583f2db3e241f50e8c50ee
Dharshan2001 <75156804+Dharshan2001@users.noreply.github.com>
2021-02-07 06:51:28 -0800

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,1 +1,9 @@
-//Write the program to print "Hello World !"
+//Write the program to print "user name"
+#include<stdio.h>
+void main()
+{
+ char name[10];
+printf("enter your name");
+scanf("%s",&name);
+printf("hi %s",name);
+}
